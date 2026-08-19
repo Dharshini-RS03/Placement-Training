6fad8522c126a92b6a4483ecb86a2fdebca225df
dharsh03rs-cpu <dharsh03rs@gmail.com>
2026-08-19 09:23:24 +0530

    Largest from 2

---
+++ large_two.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class large_two{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+
+        if (a > b)
+            System.out.println("Largest: " + a);
+        else
+            System.out.println("Largest: " + b);
+
+        sc.close();
+    }
+}
